7500756c2b4cf67ddefc9ea20d74dd132efccce1
Dan Turner <djtech42@me.com>
2022-07-06 13:15:34 -0400

    update Package.swift

--- Package.swift
+++ Package.swift
@@ -3,13 +3,13 @@
 import PackageDescription
 
 let package = Package(
-    name: "PPBadgeView",
+    name: "PPBadgeViewSwift",
     platforms: [
         .iOS(.v8)
     ],
     products: [
         // Products define the executables and libraries produced by a package, and make them visible to other packages.
-        .library(name: "PPBadgeView", targets: ["PPBadgeView"])
+        .library(name: "PPBadgeViewSwift", targets: ["PPBadgeViewSwift"])
     ],
     dependencies: [
         // Dependencies declare other packages that this package depends on.
@@ -18,7 +18,7 @@ let package = Package(
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
         // Targets can depend on other targets in this package, and on products in packages which this package depends on.
-        .target(name: "PPBadgeView", dependencies: [], path: "PPBadgeView/swift")
+        .target(name: "PPBadgeViewSwift", dependencies: [], path: "PPBadgeView/swift")
     ],
     swiftLanguageVersions: [.v5]
 )
